1aaa8d3e510a799578987b9d5148f3d51faea399
AlexAlk93 <alex.panioglo@gmail.com>
2025-07-19 17:32:44 +0300

    Initial commit

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,7 @@ public class Test {
         System.out.println("Alex Panioglo 1");
 
         System.out.println("Vezi mesajul meu?");
+
+        System.out.println("Da.Ce ati facut azi? ");
     }
 }
