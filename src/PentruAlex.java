df2738e084a0fa48d80faee10e67ba9d15f86994
Sorin Gore <soringoredev@gmail.com>
2025-07-19 17:46:43 +0300

    Add homework

---
+++ src/PentruAlex.java
@@ -0,0 +1,7 @@
+public class PentruAlex {
+    /*
+    Noroc!
+    Asa un exercitiu. O clasa simpla care are doua variabile de tip integer
+    si care spune cu ajutorul unui if else care numar este mai mare
+     */
+}
---
+++ src/PentruCristiana.java
@@ -0,0 +1,12 @@
+public class PentruCristiana {
+    /*
+    Hey salut,
+
+    Creezi un exercitiu simplu in care sa ai o variabila
+    de tip integer.
+    Adauga un if / else care sa-ti spuna daca nr din variabila este
+    pozitiv sau negativ
+
+
+     */
+}
