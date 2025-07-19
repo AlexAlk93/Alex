8ab236bb844d9639560d331efa44e5167e68ab14
Sorin Gore <soringoredev@gmail.com>
2025-07-19 17:30:39 +0300

    Add new sout

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Sorin Gore 1");
         System.out.println("Alex Panioglo 1");
+
+        System.out.println("Vezi mesajul meu?");
     }
 }
